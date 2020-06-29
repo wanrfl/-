c91a0ff30bb074d96ae5c8fcdebbbf7137b11c29
unknown <you@example.com>
2020-06-29 19:30:51 +0800

    这是严选商成的空项目

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+//import { Swipe, SwipeItem } from 'vant';
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import axios from "axios"
+import "./assets/rem.js"
+Vue.prototype.http = axios
+
+Vue.config.productionTip = false
+//Vue.use(Swipe);
+//Vue.use(SwipeItem);
+Vue.use(Vant);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+            <Ban></Ban>
+           <div class="a">
+               <p>
+                   11111111111
+               </p>
+
+           </div>
+    </div>
+</template>
+
+<script>
+import Banner from "../components/Banner"
+export default {
+name:"",
+data() {
+return {
+
+}
+},
+components:{
+    Ban:Banner
+},
+//生命周期 - 创建完成（访问当前this实例）
+created() {
+
+},
+//生命周期 - 挂载完成（访问DOM元素）
+mounted() {
+
+}
+}
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+.home{
+    .a{
+        width:200px;
+        height:200px;
+        background: #aaa;
+        p{
+            color:#f00
+        }
+    }
+}
+</style>
